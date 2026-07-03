3f2ce7f2f60b257ddf753ce4a3294329d0c45cc4
PujaMhub <pujamaity9038@gmail.com>
2026-07-03 20:54:58 +0530

    insertion

---
+++ insertion.py
@@ -0,0 +1,15 @@
+def insert(arr):
+    n=len(arr)
+    for i in range(1,n):
+        insertindex=i
+        current=arr[i]
+        for j in range(i-1,-1,-1):
+            if arr[j]>current:
+                arr[j+1]=arr[j]
+                insertindex=j
+            else:
+                break
+        arr[insertindex]=current
+    return arr
+arr=[4,53,2,34,1]
+print(insert(arr))
