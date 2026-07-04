6f78dc51a50ab0ffc9ef91b7859cc8845f25636f
PujaMhub <pujamaity9038@gmail.com>
2026-07-04 18:26:34 +0530

    sorting

--- insertion.py
+++ insertion.py
@@ -1,32 +1,15 @@
-def insert(arr):
+def insertion(arr):
     n=len(arr)
     for i in range(1,n):
-        insertindex=i
-        current=arr[i]
-        for j in range(i-1,-1,-1):
-            if arr[j]>current:
-                arr[j+1]=arr[j]
-                insertindex=j
-            else:
-                break
-        arr[insertindex]=current
-    return arr
-arr=[4,53,2,34,1]
-print(insert(arr))
-
-def insert(arr):
-    n=len(arr)
-
-    for i in range(1,n):
-        insertindex=i
+        insert_index=i
         current = arr[i]
         for j in range(i-1,-1,-1):
             if arr[j]>current:
                 arr[j+1]=arr[j]
-                insertindex=j
+                insert_index=j
             else:
                 break
-        arr[insertindex]=current
+        arr[insert_index]=current
     return arr
-arr=[3,4,3,1,3,23]
-print(insert(arr))
+arr= [5,4,2,3,1]
+print(insertion(arr))
